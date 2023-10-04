beb4221b66d41ebe6fcdc207ef897111e01967f6
Howard Gil <howardbgil@gmail.com>
2023-10-03 21:17:19 -0700

    implemented popover without shifting

--- src/components/WidgetContainer.tsx
+++ src/components/WidgetContainer.tsx
@@ -5,18 +5,21 @@ const WidgetContainer = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div>
+    <div
+      className={`flex transition-all ease-in-out duration-300 ${
+        isOpen ? "mr-48" : ""
+      }`}
+    >
+      <div className="relative flex-grow">{/* Main content */}</div>
       <div
-        className={`fixed bottom-16 right-5 p-5 bg-blue-500 text-white rounded-lg shadow-md transition-all ease-in-out duration-300 ${
-          isOpen
-            ? "w-[400px] max-h-[800px] visible opacity-100"
-            : "w-[400px] max-h-0 invisible opacity-0"
+        className={`fixed top-0 bottom-0 right-0 bg-blue-500 transition-all ease-in-out duration-300 ${
+          isOpen ? "w-80 visible opacity-100" : "w-0 invisible opacity-0"
         }`}
       >
-        {isOpen && <p>This is a fixed container</p>}
+        <p className="p-5 text-white">This is a fixed container</p>
       </div>
       <div
-        className="fixed flex items-center justify-center w-12 h-12 text-2xl text-white bg-red-500 rounded-full cursor-pointer bottom-5 right-5"
+        className="absolute flex items-center justify-center w-12 h-12 text-2xl text-white bg-red-500 rounded-full cursor-pointer bottom-3 right-3"
         onClick={() => setIsOpen(!isOpen)}
       >
         {isOpen ? "-" : "+"}
